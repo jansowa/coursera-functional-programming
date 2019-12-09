a6a5765059ee545ba554b55184937faf0cb5b261
Jan Sowa <jan.piotr.sowa@gmail.com>
2019-12-09 10:10:03 +0100

    Add Java version of Newton sqrt method with helper methods as variables inside main sqrt method

--- src/main/java/examplejava/NewtonSqrtJava.java
+++ src/main/java/examplejava/NewtonSqrtJava.java
@@ -15,10 +15,30 @@ public class NewtonSqrtJava {
 
     final static Function<Double, Double> sqrt = x -> sqrtIter.apply(1.0, x);
 
+    final static Function<Double, Double> sqrt2 = x -> {
+        final Function<Double, Boolean> isGoodEnough = guess -> Math.abs(guess * guess - x) / x < 0.001;
+
+        final Function<Double, Double> improve = guess -> (guess + x / guess) / 2;
+
+        Function<Double, Double> sqrtIter = guess -> {
+            BiFunction<BiFunction, Double, Double> sqrtIterHelper =
+                    (function, guessInside) ->
+                        isGoodEnough.apply(guessInside) ?
+                            guessInside : (double) function.apply(function, improve.apply(guessInside));
+            return sqrtIterHelper.apply(sqrtIterHelper, guess);
+        };
+        return sqrtIter.apply(1.0);
+    };
+
     public static void main(String[] args) {
         System.out.println(
                 Optional.of(2.0)
                         .map(sqrt)
+                        .get()
+        );
+        System.out.println(
+                Optional.of(2.0)
+                        .map(sqrt2)
                         .get()
         );
     }
